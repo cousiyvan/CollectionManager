b877b9ac256b4e0c612b9f07f4d9a5c705d88b37
cousiyvan <yvan.cousillas@cousijojo.com>
2016-09-29 16:23:01 +0200

    Starting with DB entity framework

---
+++ src/CollectionManager/Models/DB/GameDBMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionManager.Models.DB
+{
+    public class GameDBMapping
+    {
+        #region Properties
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int UserId { get; set; }
+        public bool Favorite { get; set; }
+        public bool Wishlist { get; set; }
+        public bool Collection { get; set; }
+        #endregion
+    }
+}
