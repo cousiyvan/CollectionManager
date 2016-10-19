c5a89df2a2e83283534815652d49bff4b4558e78
Ivan Cousillas <yvan.cousillas@windowslive.com>
2016-10-19 23:00:52 +0200

    Simplified the search but working in Ajax mode. Only thing is the switch of elements (Favorites ...) not working fine after the search ... :/

--- src/CollectionManager/wwwroot/js/site.js
+++ src/CollectionManager/wwwroot/js/site.js
@@ -1,6 +1,18 @@
 ﻿// Write your Javascript code.
 
 $(document).ready(function () {
+    //// set global ajax options:
+    //$.ajaxSetup({
+    //    beforeSend: function (xhr, status) {
+    //        // TODO: show spinner
+    //        $('#spinner').show();
+    //    },
+    //    complete: function () {
+    //        // TODO: hide spinner
+    //        $('#spinner').hide();
+    //    }
+    //});
+
     var baseUrl = {
         "games": "https://igdbcom-internet-game-database-v1.p.mashape.com/"
     };
@@ -9,14 +21,12 @@ $(document).ready(function () {
     // Switch views of the elements to display in Games view
     // check which button is clicked and slide down/up it's content and update active class
     $(".GamesMenuItems").click(function () {
-        if ($(this).attr("id") === "MyCollection")
-        {
+        if ($(this).attr("id") === "MyCollection") {
             if ($(this).hasClass("active")) {
                 $(".MyCollectionGamesContainer").slideUp();
                 $(this).removeClass("active");
             }
-            else 
-            {
+            else {
                 $(".MyCollectionGamesContainer").slideDown();
                 $(this).addClass("active");
             }
@@ -26,8 +36,7 @@ $(document).ready(function () {
                 $(".MyWishlistGamesContainer").slideUp();
                 $(this).removeClass("active");
             }
-            else
-            {
+            else {
                 $(".MyWishlistGamesContainer").slideDown();
                 $(this).addClass("active");
             }
@@ -65,49 +74,81 @@ $(document).ready(function () {
     });
 
     // Hide some components
-    $("#ReleasesDateFilterComponents").hide();
-    $("#SearchQueryDisplay").hide();
-    $("#ratingFiltersComponents").hide();
+    //$("#ReleasesDateFilterComponents").hide();
+    //$("#SearchQueryDisplay").hide();
+    //$("#ratingFiltersComponents").hide();
 
-    // Dealing with search filters
-    $("#ReleaseDateFilter").click(function () {
-        $("#ReleasesDateFilterComponents").show();
-    });
+    //// Dealing with search filters
+    //$("#ReleaseDateFilter").click(function () {
+    //    $("#ReleasesDateFilterComponents").show();
+    //});
 
-    $("#ascendantFilter").click(function () {
-        FillSearchQueryParameters("Releases Dates - ascendant filters");
-        $("#SearchQueryDisplay").show();
-        searchUrl = "games/?search=" + $("#input-search").val() + "&order=release_dates.date:asc";
-    });
-    $("#descendantFilter").click(function () {
-        FillSearchQueryParameters("Releases Dates - descendant filters");
-        $("#SearchQueryDisplay").show();
-        searchUrl = "games/?search=" + $("#input-search").val() + "&order=release_dates.date:desc";
+    //$("#ascendantFilter").click(function () {
+    //    FillSearchQueryParameters("Releases Dates - ascendant filters");
+    //    $("#SearchQueryDisplay").show();
+    //    searchUrl = "games/?search=" + $("#input-search").val() + "&order=release_dates.date:asc";
+    //});
+    //$("#descendantFilter").click(function () {
+    //    FillSearchQueryParameters("Releases Dates - descendant filters");
+    //    $("#SearchQueryDisplay").show();
+    //    searchUrl = "games/?search=" + $("#input-search").val() + "&order=release_dates.date:desc";
+    //});
+
+    $("#emptySearchTextAlert").hide();
+
+    $("#input-search").change(function () {
+        validate();
     });
 
     $("#SearchSubmit").click(function () {
-        AddToCart(searchUrl);
+        searchUrl = "games/?search=" + $("#input-search").val();
+
+        if (validate()) {
+            AddToCart(searchUrl);
+        }
     });
+
+    function validate() {
+        var res = true;
+        if ($("#input-search").val() === "") {
+            $("#emptySearchTextAlert").show();
+            res = false;
+        }
+        else {
+            $("#emptySearchTextAlert").hide();
+        }
+
+        return res;
+    }
 
     function FillSearchQueryParameters(filters) {
         var inputSearch = $("#input-search").val();
-        $("#SearchQuery").text(inputSearch + " - " + filters);
+        //$("#SearchQuery").text(inputSearch + " - " + filters);
     }
 
-    function AddToCart(searchQuery)
-    {
+    function AddToCart(searchQuery) {
         $.ajax({
             type: 'POST',
             url: '/Collection/SearchGames',
-            data: { parameterQuery: searchQuery }
-        }).done(function () {
-            $(".MyLatestReleasesGamesContainer").html("");
-            $(".MyLatestReleasesGamesContainer").load("/Collection/SearchGamesDisplay");
-            alert("Done");
+            data: { parameterQuery: searchQuery },
+            beforeSend: function () {
+
+            },
+            success: function (html) {
+                triggerLoading($(".MyLatestReleasesGamesContainer"))
+                $(".MyLatestReleasesGamesContainer").load("/Collection/SearchGamesDisplay");
+                // remove additional footer - Don't know why it appears
+                $(".body-content > footer").remove();
+            }
         });
     }
 
-    function triggerAlert(alertMessage) {}
+    function triggerLoading(container)
+    {
+        container.html("<img src='/images/ring.gif' id='spinner' alt='loading...' style='display:block;text-align:center;' />");
+    }
+
+    function triggerAlert(alertMessage) { }
     // alert(alertMessage);
     if (alertMessage !== "") {
         $(".alert-container").fadeTo(2000, 500).slideUp(500, function () {
